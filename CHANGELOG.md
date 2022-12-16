841de448abe62b32fe6a241b963e19995d00d530
Felix Kleinekathoefer <felix@felixoi.com>
2022-12-16 10:41:53 +0100

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## v0.0.1 - 2022-12-16 - Creation
+
+#### Nothworthy Changes
+
+* Initial creation of NamedFileYamlProvider based on [octodns#966](https://github.com/octodns/octodns/pull/966)
---
+++ tests/test_provider_ctodns_namedfileyaml.py
@@ -0,0 +1,15 @@
+#
+#
+#
+
+from unittest import TestCase
+
+from octodns_namedfileyaml import NamedFileYamlProvider
+
+
+class TestNamedFileYamlProvider(TestCase):
+
+    # TODO: test provider
+    def test_nothing(self):
+        self.assertTrue(True)
+        NamedFileYamlProvider
